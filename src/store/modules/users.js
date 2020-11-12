b70d60839bceeff811d0f23001457c95f599549c
igor <gg.ziplant@gmail.com>
2020-11-12 21:18:56 +0300

    fix fetch

--- src/store/modules/users.js
+++ src/store/modules/users.js
@@ -52,7 +52,7 @@ export default {
   actions: {
     async fetchUsers({ commit, state }) {
       await fetch(
-        `http://www.filltext.com/?rows=${state.limit}&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D`
+        `https://www.filltext.com/?rows=${state.limit}&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D`
       )
         .then((res) => res.json())
         .then((data) => {
@@ -62,7 +62,7 @@ export default {
         });
     },
     async sortUsers({ commit, state }, { field, asc }) {
-      let sortedUsers = await state.filteredUsers.sort(function(a, b) {
+      let sortedUsers = await state.filteredUsers.sort(function (a, b) {
         if (!asc) return a[field] > b[field] ? 1 : -1;
         else return a[field] > b[field] ? -1 : 1;
       });
